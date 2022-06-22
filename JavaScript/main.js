e972a4a4b63ecad6e0b970376091d13563f39aa6
JMHunterX <jumaruja17@gmail.com>
2022-06-22 15:42:30 -0500

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+
+// DOM
+//querySelector / querySelectorAll
+/*
+var link = document.querySelectorAll("a");
+
+link .forEach (function(link){
+       console.log(link)
+})*/
+
+let links = document.querySelectorAll(".close"); 
+
+for(let i = 0; i < links.length; i++) {
+       console.log("paso")
+}
